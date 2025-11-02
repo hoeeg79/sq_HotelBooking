d1797fe9fe96dc07a61f55a88c87795be79bba95
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2025-11-02 20:54:44 +0100

    Cucumber done

--- HotelBooking.Specs/StepDefinitions/CreateBookingStepDefinitions.cs
+++ HotelBooking.Specs/StepDefinitions/CreateBookingStepDefinitions.cs
@@ -1,37 +1,43 @@
 using System;
+using System.Threading.Tasks;
 using HotelBooking.Core;
+using HotelBooking.TestUtils.Fakes;
 using Reqnroll;
+using Xunit;
 
 [Binding]
 public sealed class CreateBookingStepDefinitions
 {
+    private BookingManager bookingManager;
+    private readonly BookingFixture _fixture;
     private Booking _booking;
+    private bool _result;
     
+    public CreateBookingStepDefinitions(BookingFixture fixture)
+    {
+        _fixture = fixture;
+        bookingManager = new BookingManager(_fixture.BookingRepositoryMock.Object, _fixture.RoomRepositoryMock.Object);
+    }
+
     [Given("the hotel has an room available from {int} to {int}")]
     public void GivenTheHotelHasAnRoomAvailableFromTo(int startOffset, int endOffset)
     {
-        Booking booking = new Booking
+        _booking = new Booking
         {
             StartDate = DateTime.Today.AddDays(startOffset),
             EndDate = DateTime.Today.AddDays(endOffset)
         };
-        
-        // TODO: Make the booking manager with mock objects. See BookingManagerTests.cs for reference.
     }
 
     [When("the customer attempts to create the booking")]
-    public void WhenTheCustomerAttemptsToCreateTheBooking()
+    public async Task WhenTheCustomerAttemptsToCreateTheBooking()
     {
-        // TODO: Implement act step. See BookingManagerTests.cs for reference.
-        ScenarioContext.StepIsPending();
+        _result = await bookingManager.CreateBooking(_booking);
     }
 
     [Then("the booking should be successfully created")]
     public void ThenTheBookingShouldBeSuccessfullyCreated()
     {
-        // TODO: Implement assert step. See BookingManagerTests.cs for reference.
-        ScenarioContext.StepIsPending();
+        Assert.True(_result);
     }
-
-
 }
